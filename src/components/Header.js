e18b2ba24541deae5e76908a37abf72c97deac03
iyashjayesh <180303105046@paruluniversity.ac.in>
2020-12-01 14:54:01 +0530

    Header Changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
-import { Icon } from '@iconify/react'
-import locationIcon from '@iconify/icons-mdi/fire-alert'
+// npm install --save-dev @iconify/react @iconify-icons/flat-ui
+import { Icon } from '@iconify/react';
+import earthIcon from '@iconify-icons/flat-ui/earth';
+
 
 const Header = () => {
     return (
         <header className="header">
-            <h1><Icon icon={locationIcon} />NASA-Earth-Observatory-Natural-Event-Tracker (Project build by @iyashjayesh) </h1>
+            <h1><Icon icon={earthIcon} /> NASA-Earth-Observatory-Natural-Event-Tracker (Project build by <a href="https://www.linkedin.com/in/iyashjayesh/">@iyashjayesh</a>) </h1>
         </header>
     )
 }
